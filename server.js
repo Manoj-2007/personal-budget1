d2da9e825f32521c7df0c42fbca4d428ad2240ff
Manoj-2007 <93203846+Manoj-2007@users.noreply.github.com>
2024-02-03 19:33:07 -0500

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-const express= require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/",express.static('public'));
+const budget = require('./budgetData.json');
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.use("/", express.static('public'))
 
-
-app.get('/hello',(req,res) => {
-    res.send('Hello World!');
-});
-
-app.get('/budget', (req, res) => {
+app.get("/budget", (req, res) => {
     res.json(budget);
-});
+})
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
-});
+    console.log('Listening to http://localhost:${port}')
+})
